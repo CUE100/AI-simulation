b9af73ab21c9fff15185e65fb4af5b5128ba68c9
cue <onetapfact@gmail.com>
2026-06-18 21:14:50 +0530

    commit

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/dakshbeniwal/codemolde/AI-simulation
+# Install script for directory: /Users/rohinboora/code/AI-simulation
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/dakshbeniwal/codemolde/AI-simulation/build/install_local_manifest.txt"
+  file(WRITE "/Users/rohinboora/code/AI-simulation/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/dakshbeniwal/codemolde/AI-simulation/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/rohinboora/code/AI-simulation/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
