72fec5b8b4043c593be425b8e3eca551fe447dc1
codemolde <techzgaming36@gmail.com>
2026-06-18 21:01:18 +0530

    CmakeLists

---
+++ build/CMakeFiles/ai_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_simulation.dir/engine/main.cpp.o"
+  "CMakeFiles/ai_simulation.dir/engine/main.cpp.o.d"
+  "ai_simulation"
+  "ai_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
