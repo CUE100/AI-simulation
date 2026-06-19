5bd4854cee8c08021c038643e2105980f0930d69
cue <onetapfact@gmail.com>
2026-06-19 18:14:20 +0530

    commit

--- Python/agent.py
+++ Python/agent.py
@@ -4,7 +4,11 @@ import numpy as np
 import torch.optim as optim
 from torch.nn import LSTM, GRU
 from torch.nn import Conv2d, MaxPool2d
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
 
-class AIbrain(nn.module):
+class AIbrain(nn.Module):
     def __init__(self):
+        super().__init__()
