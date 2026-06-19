1d65e6ddbac4f60e0b7dbcc70a8612b1dccf808d
cue <onetapfact@gmail.com>
2026-06-19 18:38:01 +0530

    commit

--- Python/agent.py
+++ Python/agent.py
@@ -1,4 +1,4 @@
-import torch    
+import torch
 from torch import nn
 import numpy as np
 import torch.optim as optim
@@ -12,9 +12,14 @@ device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 class AIbrain(nn.Module):
     def __init__(self):
         super().__init__()
-        self.visual = visual
-        self.sound = sound
-        self.sense = sense
-        self.chemical = chemical
+        self.visual = None
+        self.sound = None
+        self.sense = None
+        self.chemical = None
 
-    def visual():
+    def visual(self, map_data):
+        matrix_array = np.array(raw_map_grid, dtype=np.float32)
+        visual_tensor = torch.tensor(matrix_array, dtype=torch.float32)
+        visual_tensor = visual_tensor.permute(2, 0, 1)
+        final_visual_matrix = visual_tensor.unsqueeze(0)
+        return final_visual_matrix
